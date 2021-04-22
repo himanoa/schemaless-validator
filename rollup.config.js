04ff30f787d08073c4830ffe886c3329c26e3de8
himanoa <matsunoappy@gmail.com>
2021-04-23 05:18:21 +0900

    Fix a rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -2,12 +2,12 @@ import typescript from 'rollup-plugin-typescript2';
 
 const config = [
   {
-    input: "./src/index.ts",
+    input: "./src/lib.ts",
     output: [{ file: "lib/index-esm.js", format: "es" }],
     plugins: [typescript({ declarationDir: "./lib"})],
   },
   {
-    input: "./src/index.ts",
+    input: "./src/lib.ts",
     output: [{ file: "lib/index-cjs.js", format: "cjs", exports: "named" }],
     plugins: [typescript()],
   }
